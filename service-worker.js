99475a90ce224ac834d26656c3671c09946d7f56
mohsen <abdmohsen94@gmail.com>
2026-09-01 21:01:47 +0300

    تحديث الجزء والحزب

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* Quran PWA Service Worker */
-const VERSION = 'v1.2.0';
+const VERSION = 'v1.2.1';
 const STATIC_CACHE    = `quran-static-${VERSION}`;
 const RUNTIME_CACHE   = `quran-runtime-${VERSION}`;
 const AUDIO_CACHE     = `quran-audio-${VERSION}`;
